04ad15b85e94b1283172c640660805b41ce98c21
Eric Jessé <me@ericjesse.com>
2022-09-16 15:44:43 +0200

    Initialization of the repository

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# qalipsis-user-bom
-BOM module for the creation of QALIPSIS scenarios
+# QALIPSIS BOM
+
+Platform dependencies to develop scenarios for QALIPSIS.
+
+## How to use it?
+
+In your build.gradle file, add the following dependency to your scen:
+
+```
+dependencies {
+    implementation(enforcedPlatform("io.qalipsis:qalipsis-platform:<version>"))
+    kapt(enforcedPlatform("io.qalipsis:qalipsis-platform:<version>"))
+    
+    // Add your plugins:
+    implementation("io.qalipsis:plugin-cassandra")
+    implementation("io.qalipsis:plugin-jackson")
+    
+    // Add the additional libraries you need:
+    implementation("io.kotest:kotest-assertions-core:5.4.2")
+}
+```
+
+That's all folk!
+
+## Current version
+
+To know the current version, look at [this file](./project.version).
---
+++ build.gradle.kts
@@ -0,0 +1,111 @@
+plugins {
+    `java-platform`
+
+    id("nebula.contacts") version "5.1.0"
+    id("nebula.info") version "9.1.1"
+    id("nebula.maven-publish") version "17.0.0"
+    id("nebula.maven-scm") version "17.0.0"
+    id("nebula.maven-manifest") version "17.0.0"
+    signing
+}
+
+group = "io.qalipsis"
+version = File(rootDir, "project.version").readText().trim()
+
+infoBroker {
+    excludedManifestProperties = listOf(
+        "Manifest-Version", "Module-Owner", "Module-Email", "Module-Source",
+        "Built-OS", "Build-Host", "Build-Job", "Build-Host", "Build-Job", "Build-Number", "Build-Id", "Build-Url",
+        "Built-Status"
+    )
+}
+
+contacts {
+    addPerson("eric.jesse@aeris-consulting.com", delegateClosureOf<nebula.plugin.contacts.Contact> {
+        moniker = "Eric Jessé"
+        github = "ericjesse"
+        role("Owner")
+    })
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    maven {
+        name = "maven-central-snapshots"
+        setUrl("https://oss.sonatype.org/content/repositories/snapshots")
+    }
+}
+
+signing {
+    publishing.publications.forEach { sign(it) }
+}
+
+val ossrhUsername: String? by project
+val ossrhPassword: String? by project
+publishing {
+    repositories {
+        maven {
+            val releasesRepoUrl = "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
+            val snapshotsRepoUrl = "https://oss.sonatype.org/content/repositories/snapshots/"
+            name = "sonatype"
+            // See https://docs.gradle.org/current/userguide/single_versions.html#version_ordering.
+            url = uri(if (project.version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
+            credentials {
+                username = ossrhUsername
+                password = ossrhPassword
+            }
+        }
+    }
+
+    publications {
+        create<MavenPublication>("qalipsis-bom") {
+            from(components["javaPlatform"])
+        }
+    }
+}
+
+val kotlin = "1.6.21"
+val core = "0.4.27-SNAPSHOT"
+val kotlinCoroutines = "1.6.4"
+
+javaPlatform {
+    allowDependencies()
+}
+
+dependencies {
+    // Platform modules.
+    api("org.jetbrains.kotlin:kotlin-stdlib:$kotlin")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:${kotlinCoroutines}")
+
+    // Core modules.
+    api("io.qalipsis:api-dsl:$core")
+    api("io.qalipsis:api-processors:$core")
+
+    // Libraries that could be brought by other dependencies and break the compilation or execution.
+    api("ch.qos.logback:logback-classic") { version { strictly("1.2.3") } }
+    api("ch.qos.logback:logback-core") { version { strictly("1.2.3") } }
+
+    constraints {
+        // Core modules.
+        runtime("io.qalipsis:runtime:$core")
+        runtime("io.qalipsis:head:$core")
+        runtime("io.qalipsis:factory:$core")
+
+        // Plugins.
+        api("io.qalipsis:plugin-cassandra:0.4.20-SNAPSHOT")
+        api("io.qalipsis:plugin-elasticsearch:0.4.20-SNAPSHOT")
+        api("io.qalipsis:plugin-graphite:0.4.20-SNAPSHOT")
+        api("io.qalipsis:plugin-influxdb:0.4.20-SNAPSHOT")
+        api("io.qalipsis:plugin-jackson:0.4.20-SNAPSHOT")
+        api("io.qalipsis:plugin-jms:0.4.20-SNAPSHOT")
+        api("io.qalipsis:plugin-kafka:0.4.20-SNAPSHOT")
+        api("io.qalipsis:plugin-mongodb:0.4.20-SNAPSHOT")
+        api("io.qalipsis:plugin-netty:0.4.20-SNAPSHOT")
+        api("io.qalipsis:plugin-r2dbc-jasync:0.4.20-SNAPSHOT")
+        api("io.qalipsis:plugin-rabbitmq:0.4.20-SNAPSHOT")
+        api("io.qalipsis:plugin-redis-lettuce:0.4.20-SNAPSHOT")
+        api("io.qalipsis:plugin-timescaledb:0.4.21-SNAPSHOT")
+    }
+}
+
